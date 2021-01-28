a76d5b26d7851b3196abe7c9eec299fc476319fa
Senthuran <senthuran@blackvt.com>
2021-01-28 15:29:49 +0530

    Api URL configured with  proxy

--- src/services/api-calls.js
+++ src/services/api-calls.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://ivivaanywhere.ivivacloud.com/api";
+const baseURL = "api";
 const APIKEY = "SC:demo:64a9aa122143a5db";
 
 const instance = axios.create({
